3837062d02638c0a4f2243fdf4db0d75b9b25bb2
ncRakl <nathan.raklure@gmail.com>
2020-02-04 00:18:47 +0100

    starting files for a tic tac toe

---
+++ ticTacToe/Grille.h
@@ -0,0 +1,12 @@
+
+class Grille
+{
+public:
+	Grille();
+
+	void display() const;
+	void start();
+
+private:
+	char grille[3][3];
+};
---
+++ ticTacToe/main.cpp
@@ -0,0 +1,9 @@
+#include "Grille.h"
+
+int main(int argc, char const *argv[])
+{
+	Grille g;
+	g.display();
+
+	return 0;
+}
